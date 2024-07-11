7ebcce042ffcd46bc915f3738b405a9273b01d0b
vanshika704 <sharmavanshi704@gmail.com>
2024-07-11 10:53:30 +0530

    object creation

---
+++ objects.java
@@ -0,0 +1,13 @@
+public class objects {
+    void abc(){
+        System.out.print("hello");
+    }
+    public static void main(String[] args) {
+        objects obj = new objects();
+        obj.abc();
+        
+    }
+
+
+    
+}
