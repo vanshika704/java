9b7b07652bb3a0c7aadc512832b2d1e28230b39e
vanshika704 <sharmavanshi704@gmail.com>
2024-08-04 00:55:03 +0530

    delete

--- strings.java
+++ strings.java
@@ -6,6 +6,9 @@ public class strings {
         StringBuffer str2 = new StringBuffer(str+str1);
         str2.append("    How are you?");
         System.out.println(str2);
-     
+     str2.insert(3, 0);
+     System.out.println(str2);
+     str2.delete(3, 4);
+     System.out.println(str2);
     }
 }
