6c752eebbc550030a453cfa85e31f86cd709a816
vanshika704 <sharmavanshi704@gmail.com>
2024-09-18 23:14:55 +0530

    tg nlet

---
+++ assignment1/SimplePrimeCheck.java
@@ -0,0 +1,33 @@
+package assignment1;
+import java.util.Scanner;
+
+public class SimplePrimeCheck {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+        
+        scanner.close();
+    }
+
+    // Simple method to check if a number is prime
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
