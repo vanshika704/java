864cb5570413953febce9a2080c9efb7794fe820
vanshika704 <sharmavanshi704@gmail.com>
2024-09-18 23:53:14 +0530

    s

--- practice/Threads.java
+++ practice/Threads.java
@@ -11,34 +11,34 @@ class NumberThread extends Thread {
         }
     }
 }
-class LetterThread extends Thread {
+// class LetterThread extends Thread {
 
-    @Override
-    public void run (){
-        char [] letters = {'A','B','C','D','E'};
-     for (char letter : letters) {
-        System.out.println("Letter: " + letter);
-        try {
-            Thread.sleep(1500); // Sleep for 1.5 seconds
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }}
-}
+//     @Override
+//     public void run (){
+//         char [] letters = {'A','B','C','D','E'};
+//      for (char letter : letters) {
+//         System.out.println("Letter: " + letter);
+//         try {
+//             Thread.sleep(1500); // Sleep for 1.5 seconds
+//         } catch (InterruptedException e) {
+//             e.printStackTrace();
+//         }
+//     }}
+// }
 public class Threads  {
     public static void main(String[] args) {
         // Create instances of each thread
         NumberThread numberThread = new NumberThread();
-        LetterThread letterThread = new LetterThread();
+        // LetterThread letterThread = new LetterThread();
 
         // Start both threads
         numberThread.start();
-        letterThread.start();
+        // letterThread.start();
 
         // Wait for both threads to finish
         try {
             numberThread.join();
-            letterThread.join();
+            // letterThread.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
