d25759bdfadf10d3135311dd4720f6185ecdb301
vanshika704 <sharmavanshi704@gmail.com>
2024-09-24 19:38:56 +0530

    300 guyzzzzz

--- arrays.java
+++ arrays.java
@@ -1,7 +1,13 @@
 public class arrays {
     public static void main(String[] args) {
-        int array[];
+       
+        int[] array = new int[5];
+    
+        array = new int[]{45, 56, 56, 46, 45};
         
-
+       
+        for (int i : array) {
+            System.out.println(i);
+        }
     }
 }
