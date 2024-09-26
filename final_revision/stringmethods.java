caf2d41944f97f4147372ec7a243ad4e409405cd
vanshika704 <sharmavanshi704@gmail.com>
2024-09-26 21:06:29 +0530

    heheheh

--- final_revision/stringmethods.java
+++ final_revision/stringmethods.java
@@ -1,11 +1,15 @@
 public class stringmethods {
     public static void main(String[] args) {
+        if (args.length == 0) {
+            return; // Exit if no arguments are provided
+        }
+
         String str1 = args[0];
 
-        str1.length();
-        str1.substring(2,4);
-        str1.trim();
-        str1.charAt(4);
-        str1.concat("munjal sir ki paint me"); // bro dont judge ... ye sirf college romance ka dialogue h
+      System.out.println(str1.length());  
+      System.out.println( str1.substring(2, 4)); 
+      System.out.println(str1.trim());   
+       System.out.println( str1.charAt(4));
+       System.out.println( str1.concat(" munjal sir ki paint me"));
     }
 }
