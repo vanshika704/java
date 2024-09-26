c2f414e4d9488bf2f6ea647d20a802b287425728
vanshika704 <sharmavanshi704@gmail.com>
2024-09-26 21:28:41 +0530

    heueheueu

--- final_revision/stringmethods.java
+++ final_revision/stringmethods.java
@@ -5,11 +5,19 @@ public class stringmethods {
         }
 
         String str1 = args[0];
+        
 
       System.out.println(str1.length());  
       System.out.println( str1.substring(2, 4)); 
       System.out.println(str1.trim());   
        System.out.println( str1.charAt(4));
        System.out.println( str1.concat(" kya haal bachha log "));
+
+       StringBuilder str2 = new StringBuilder("Hello");
+     System.out.println(str2.append(" baccha "));
+     System.out.println(str2.toString());
+     System.out.println(str2.charAt(3));
+     System.out.println(str2.delete(2,5));
+ System.out.println(str2.length());
     }
 }
