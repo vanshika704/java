94ed575247afe1371c9e3839f4b8830795c92390
vanshika704 <sharmavanshi704@gmail.com>
2024-10-05 22:04:51 +0530

    hfj

--- fulljava/java Networking/clientClass.java
+++ fulljava/java Networking/clientClass.java
@@ -5,9 +5,9 @@ import java.net.*;
 public class clientClass {
     public static void main(String[] args) {
         try{
-            Socket s = new Socket("localhost",6666);
-            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
-            dout.writeUTF("Hor bhyi hogya connect aagye swaad?");
+            Socket s = new Socket("localhost",6666);// socket ka object bnate h 
+            DataOutputStream dout = new DataOutputStream(s.getOutputStream()); // dataoutptstream chlate h 
+            dout.writeUTF("Hor bhyi hogya connect aagye swaad?"); // usko value dete  h
             dout.flush();
             dout.close();
             s.close();
