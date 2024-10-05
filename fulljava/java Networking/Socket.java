290571e31deb37572be1101faf7216692f5f5cf7
vanshika704 <sharmavanshi704@gmail.com>
2024-10-05 21:37:47 +0530

    sockett defination

---
+++ fulljava/java Networking/Socket.java
@@ -0,0 +1,3 @@
+//Java Socket programming is used for communication between the applications running on different JRE.
+//Socket class
+// A socket is simply an endpoint for communications between the machines. The Socket class can be used to create a socket.
