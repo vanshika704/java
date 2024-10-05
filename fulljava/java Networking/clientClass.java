d7eed14372a88003292f5b073992af733ad87568
vanshika704 <sharmavanshi704@gmail.com>
2024-10-05 22:00:07 +0530

    jdcn

--- fulljava/java Networking/clientClass.java
+++ fulljava/java Networking/clientClass.java
@@ -1,5 +1,19 @@
+import java.io.*;
+import java.net.*;
 
 
 public class clientClass {
+    public static void main(String[] args) {
+        try{
+            Socket s = new Socket("localhost",6666);
+            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+            dout.writeUTF("Hor bhyi hogya connect aagye swaad?");
+            dout.flush();
+            dout.close();
+            s.close();
+        }catch(Exception e){
+            System.out.println("Exception generated " + e);
+        }
+    }
     
 }
