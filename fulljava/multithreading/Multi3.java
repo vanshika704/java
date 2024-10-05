aa333f6aa3b2df1e2af0595f58e21df0ce40a7e9
vanshika704 <sharmavanshi704@gmail.com>
2024-10-05 23:30:53 +0530

    gtf

--- fulljava/multithreading/Multi3.java
+++ fulljava/multithreading/Multi3.java
@@ -26,3 +26,18 @@ class lifecycle extends Thread {
         t1.start();
     }
 }
+
+// public class MyThread1  
+// {  
+
+// public static void main(String argvs[])  
+// {  
+
+// Thread t= new Thread("My first thread");  
+   
+// t.start();  
+
+// String str = t.getName();  
+// System.out.println(str);  
+// }  
+// }  
