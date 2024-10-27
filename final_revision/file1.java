d95914225b2951b2555aa4d62864d91ce07bd736
vanshika704 <sharmavanshi704@gmail.com>
2024-10-27 22:05:53 +0530

    ccd

---
+++ final_revision/file1.java
@@ -0,0 +1,16 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class FileReadExample {
+    public static void main(String[] args) {
+        try (FileInputStream fis = new FileInputStream("file.txt")) {
+            int content;
+            while ((content = fis.read()) != -1) {
+                System.out.print((char) content);
+            }
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
