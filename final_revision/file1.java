86abb1c509f5fc63121db34c75f398832cd48f07
vanshika704 <sharmavanshi704@gmail.com>
2024-10-27 22:27:16 +0530

    sab kuch

--- final_revision/file1.java
+++ final_revision/file1.java
@@ -1,14 +1,14 @@
 import java.io.FileInputStream;
 import java.io.IOException;
 
-public class FileReadExample {
+public class file1 {
     public static void main(String[] args) {
         try (FileInputStream fis = new FileInputStream("file.txt")) {
             int content;
             while ((content = fis.read()) != -1) {
                 System.out.print((char) content);
             }
-            
+
         } catch (IOException e) {
             e.printStackTrace();
         }
