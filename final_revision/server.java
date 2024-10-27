050421873626607bd49e1024439cce9daf5ddeb1
vanshika704 <sharmavanshi704@gmail.com>
2024-10-27 17:39:07 +0530

    ayeayeaye

--- final_revision/server.java
+++ final_revision/server.java
@@ -5,7 +5,7 @@ public class server {
     public static void main(String[] args) {
         try {
             ServerSocket ss = new ServerSocket(1156);
-            System.out.println("Server is running...");
+          
             Socket s = ss.accept();
             String str = "Hello from the server";
             
