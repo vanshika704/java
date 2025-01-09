56967aa56546674a03369a5e25a3684d1541595a
vanshika704 <sharmavanshi704@gmail.com>
2025-01-09 15:22:41 +0530

    aoise jkfnrf

--- fulljava/CountingSortWithSteps.java
+++ fulljava/CountingSortWithSteps.java
@@ -4,35 +4,41 @@ public class CountingSortWithSteps {
     public static void countingSort(int[] arr) {
         int steps = 0;
 
+       // finding the max element , arrays.stream krke hrr element ko ek ek krke traverse ktrta h or max number ko nikal deta h
         int max = Arrays.stream(arr).max().orElse(0);
-        steps += arr.length;
+      
 
-        int[] count = new int[max + 1];
-        steps += max + 1;
+   
+        int[] count = new int[max + 1];// ek array bnaya count check krne ke liye 
+        steps++;
 
-        for (int num : arr) {
+   
+        for (int num : arr) { // for loop lgaya h jisme hrr element ko ek ek krke count array me add kr rha h
             count[num]++;
             steps++;
         }
 
+   
         for (int i = 1; i < count.length; i++) {
             count[i] += count[i - 1];
             steps++;
         }
 
+  
         int[] output = new int[arr.length];
         for (int i = arr.length - 1; i >= 0; i--) {
             output[count[arr[i]] - 1] = arr[i];
             count[arr[i]]--;
-            steps += 2;
+            steps++;
         }
 
+     
         System.arraycopy(output, 0, arr, 0, arr.length);
-        steps += arr.length;
+        steps++;
 
+     
         System.out.println("Sorted Array: " + Arrays.toString(arr));
         System.out.println("Total Steps: " + steps);
-        System.out.println("Expected Steps: " + (5 * max + 4 * arr.length + 6));
     }
 
     public static void main(String[] args) {
